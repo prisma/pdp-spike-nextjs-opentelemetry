f25de05817357a9495af5c52927afed8850c2a22
Jason Kuhrt <jasonkuhrt@me.com>
2021-08-02 15:39:15 -0400

    initial commit

---
+++ trace.js
@@ -0,0 +1,18 @@
+// import { CollectorTraceExporter } from '@opentelemetry/exporter-collector'
+
+const { NodeTracerProvider } = require('@opentelemetry/node')
+
+NodeTracerProvider
+
+// CollectorTraceExporter
+// const exporter = new CollectorTraceExporter()
+
+// const provider = new NodeTracerProvider()
+
+// provider.addSpanProcessor(new SimpleSpanProcessor(exporter))
+// provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()))
+// provider.register()
+
+// registerInstrumentations({
+//   instrumentations: [new GraphQLInstrumentation()],
+// })
