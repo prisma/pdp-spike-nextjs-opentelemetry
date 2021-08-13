83a1a5c69567ed55d059ca1fd347fc715bf8cfce
Jason Kuhrt <jasonkuhrt@me.com>
2021-08-13 12:11:49 -0400

    more complicated example work

--- demos/service-2.ts
+++ demos/service-2.ts
@@ -36,15 +36,58 @@ const tracer = otel.trace.getTracer('request')
 main()
 
 async function main() {
-  // TODO what does this do?
-  otel.diag.info('what will happen?')
-
-  await tracer.startActiveSpan('op0', async (span) => {
+  await tracer.startActiveSpan('0', async (_0span) => {
     await Timers.setTimeout(100)
-    await tracer.startActiveSpan('op0sub0', async (span) => {
+
+    // console.log(otel.context.active())
+
+    await tracer.startActiveSpan('0.0', async (_00span) => {
       await Timers.setTimeout(100)
-      span.end()
+
+      // console.log(otel.context.active())
+
+      const _000context = otel.trace.setSpan(otel.context.active(), _00span)
+      const _000span = tracer.startSpan('0.0.0', {}, _000context)
+      _000span.end()
+
+      const _001context = otel.context.active()
+      const _001span = tracer.startSpan('0.0.1', {}, _001context)
+      _001span.end()
+
+      const _002context = otel.context.active()
+      const _002span = tracer.startSpan('0.0.2', {}, _002context)
+      _002span.end()
+
+      const _003span = tracer.startSpan('0.0.3', {})
+      _003span.end()
+
+      _00span.end()
     })
-    span.end()
+
+    const _01span = tracer.startSpan('0.1', {})
+    _01span.end()
+
+    const _02span = tracer.startSpan('0.2', {})
+    const _02context = otel.trace.setSpan(otel.context.active(), _02span)
+
+    otel.context.with(_02context, () => {
+      // console.log(otel.context.active())
+      const _020span = tracer.startSpan('0.2.0', {})
+      _020span.end()
+    })
+
+    _02span.end()
+
+    const _03span = tracer.startSpan('0.3', {})
+    const _03context = otel.trace.setSpan(otel.context.active(), _03span)
+    await otel.context.with(_03context, async () => {
+      await tracer.startActiveSpan('0.3.0', async (_030span) => {
+        await Timers.setTimeout(100)
+        _030span.end()
+      })
+    })
+    _03span.end()
+
+    _0span.end()
   })
 }
